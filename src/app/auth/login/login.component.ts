1f0539a29995d709629077a955c33f77f9a6507a
Ignacio Araya <vicariousmen@gmail.com>
2022-10-02 22:08:45 -0300

    Se hicieron validaciones con jquery-validation en los componentes login y register

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,24 +1,64 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-
+declare var $ : any;
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  loginForm: FormGroup = this._formBuilder.group({
-    email: this._formBuilder.control({value:'', disabled: false}, [Validators.required, Validators.email]),
-    password: this._formBuilder.control({value:'', disabled: false}, [Validators.required]),
-  });
+  
   constructor(
     private _formBuilder: FormBuilder,
     private router : Router
   ) { }
 
   ngOnInit(): void {
+    $(function() {
+      //$("#login").prop("disabled", true)
+      if($("#login").length) {
+        $("#email").blur(function() {
+          if ($("#login").valid() == false) {
+            $("#bLogin").prop("disabled", true);
+            return; 
+          }
+          $("#bLogin").prop("disabled", false);
+          return;
+        })
+        $("#password").blur(function() {
+          if ($("#login").valid() == false) {
+            $("#bLogin").prop("disabled", true);
+            return; 
+          }
+          $("#bLogin").prop("disabled", false);
+          return;
+        })
+        $("#login").validate({
+          rules: {
+            email: {
+              required: true,
+              email: true
+            },
+            password: {
+              required: true
+            }
+          },
+          messages: {
+            email: {
+              required: 'Por favor llene el campo Correo Electrónico!',
+              //error message for the email field
+              email: 'Por favor llene el campo con un correo valido!'
+            },
+            password: {
+              required: 'Por favor llene el campo Contraseña!'
+            }
+          }
+        });
+      }
 
+    });
+    
   }
 
   doRegist() {
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,72 +1,111 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import * as $ from 'jquery';
+declare var $ : any;
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
-  registForm: FormGroup = this._formBuilder.group({
+  /*registForm: FormGroup = this._formBuilder.group({
     nickname: this._formBuilder.control({value:'', disabled: false}, [Validators.required]),
     name: this._formBuilder.control({value:'', disabled: false}, [Validators.required]),
     lastName: this._formBuilder.control({value:'', disabled: false}, [Validators.required]),
     email: this._formBuilder.control({value:'', disabled: false}, [Validators.required, Validators.email]),
     password: this._formBuilder.control({value:'', disabled: false}, [Validators.required]),
-  });
-  get nickname() {
-    return this.registForm.get('nickname');
-  }
+  });*/
   constructor(
     private _formBuilder: FormBuilder,
     private router : Router
     ) { }
 
   ngOnInit(): void {
-    //var expr = /^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/;
-    $(document).ready(function(){
-      $("#bRegistrarme").click(function(){
-        var nickname = $("#nickname").val();
-        var name = $("#name").val();
-        var lastName = $("#lastName").val();
-        var email = $("#email").val();
-        var password = $("#password").val();
-        if (nickname == "") {
-          console.log(nickname);
-          $("#msjNickname").fadeIn();
-          return false;
-        } else {
-          console.log(nickname);
-          $("msjNickname").fadeOut();
-          if (name == "") {
-            $("#msjName").fadeIn();
-            return false;
-          } else {
-            $("msjName").fadeOut();
-            if (lastName == "") {
-              $("#msjLastName").fadeIn();
-              return false;
-            } else {
-              $("msjLastName").fadeOut();
+    $(function() {
+      //$("#login").prop("disabled", true)
+      if($("#registration").length) {
+        $("#nickname").blur(function() {
+          if ($("#registration").valid() == false) {
+            $("#bRegistrarme").prop("disabled", true);
+            return; 
+          }
+          $("#bRegistrarme").prop("disabled", false);
+          return;
+        });
+        $("#name").blur(function() {
+          if ($("#registration").valid() == false) {
+            $("#bRegistrarme").prop("disabled", true);
+            return; 
+          }
+          $("#bRegistrarme").prop("disabled", false);
+          return;
+        });
+        $("#lastName").blur(function() {
+          if ($("#registration").valid() == false) {
+            $("#bRegistrarme").prop("disabled", true);
+            return; 
+          }
+          $("#bRegistrarme").prop("disabled", false);
+          return;
+        });
+        $("#email").blur(function() {
+          if ($("#registration").valid() == false) {
+            $("#bRegistrarme").prop("disabled", true);
+            return; 
+          }
+          $("#bRegistrarme").prop("disabled", false);
+          return;
+        })
+        $("#password").blur(function() {
+          if ($("#registration").valid() == false) {
+            $("#bRegistrarme").prop("disabled", true);
+            return; 
+          }
+          $("#bRegistrarme").prop("disabled", false);
+          return;
+        });
+        $("#registration").validate({
+          rules: {
+            nickname: {
+              required: true
+            },
+            name: {
+              required: true
+            },
+            lastName: {
+              required: true
+            },
+            email: {
+              required: true,
+              email: true
+            },
+            password: {
+              required: true
             }
-            if (email == "") {
-              $("#msjEmail").fadeIn();
-              return false;
-            } else {
-              $("msjEmail").fadeOut();
-              if (password == "") {
-                $("#msjPassword").fadeIn();
-                return false;
-              } else {
-                $("msjPassword").fadeOut();
-              }
+          },
+          messages: {
+            nickname: {
+              required: 'Por favor llene el campo Apodo!'
+            },
+            name: {
+              required: 'Por favor llene el campo Nombre!'
+            },
+            lastName: {
+              required: 'Por favor llene el campo Apellido!'
+            },
+            email: {
+              required: 'Por favor llene el campo Correo Electrónico!',
+              //error message for the email field
+              email: 'Por favor llene el campo con un correo valido!'
+            },
+            password: {
+              required: 'Por favor llene el campo Contraseña!'
             }
           }
-        }
-        return true;
-      })
-    })
+        });
+      }
+
+    });
   }
   doRegist() {
     this.router.navigate(['/auth/login']);
